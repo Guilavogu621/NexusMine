3a4775b0ce49333eeedefe349a1244396de0c5bf
Lux <204750995+Guilavogu621@users.noreply.github.com>
2026-02-25 13:13:52 +0000

    Add auto-creation of admin user

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,6 @@ python3 -m pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
+# Création automatique de l'admin (ne fais rien si l'admin existe déjà)
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='eliot@gmail.com').exists() or User.objects.create_superuser('admin@nexusmine.com', 'MR.Robot', first_name='Lux', last_name='Guilavogui')"
+    
